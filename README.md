010c21f13757e793da15a6177592901c0d8ce383
Marcu Leancă <marcul@tutanota.com>
2024-06-29 13:41:22 +0300

    improved resizing algorithm

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ https://github.com/tentomush1/img2ascii/assets/79970549/4c7f7063-6743-4137-8e9b-
 # Yet another image to ASCII converter.
 I made this to improve my skills.
 
-## Version 1.0 is out in the releases guys!
+## Version 1.1 is out in the releases guys!
 
 The release is not for Windows
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from PIL import Image, ImagePalette
+from PIL import Image, ImagePalette, ImageOps
 
-ascii_chars = list("$B%&W#*oakbdqmZOQLCUXcvnxjf/|)1}[]-_~<>!lI;:,^`'. ")
+ascii_chars = list("$B%&W#*oakbdpqmZOQLCUXcvnxjf/|)1}[]-_~<>!lI;:,^`'. ")
 index = 0
 
 print('If you get an error in image location use absolute path. Otherwise use ./[...] path')
 
 # open image
-# i = Image.open("/home/m/cs/python/img2ascii/input.jpg") #fixed path
-i = Image.open(input("Image location: \n"))
+# i = Image.open("/home/m/cs/python/img2ascii/3.png") #fixed path
+i = Image.open(input("Image location: "))
 # ask for image size
-size = input("ASCII art max width/height size (in px): \n")
+size = input("ASCII art max width/height size (in characters): ")
 size = int(size)
+size_tuple = (size, size)
 # resize image
 width, height = i.size
 width = width*2
@@ -21,6 +22,7 @@ else:
     width = int(size / (height / width))
     height = size
 i = i.resize((width,height))
+
 # convert rgb to grayscale
 i = i.convert("L")
 
@@ -30,9 +32,9 @@ tuple_list = list(i.getdata())
 # write to the output file
 o = open('output.txt', 'w')
 
-while index < len(tuple_list):
-    if index % size == 0 and index != 0:
-        o.write('\n')
+while index < len(tuple_list): 
+    if index % width == 0 and index != 0:
+        o.write('\n')          
     o.write(ascii_chars[int((tuple_list[index])/5)-1])
     index += 1
 
@@ -43,11 +45,4 @@ o.close()
 # print file output
 o = open('output.txt', 'r')
 print(o.read())
-
-
-
-
-
-
-
-
+o.close()
